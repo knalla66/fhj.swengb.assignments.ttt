809e6aefaf12cf5a4a44cf91a2c18be388347d6e
knalla66 <markus.knaller@edu.fh-joanneum.at>
2015-12-11 15:23:10 +0100

    first try --> board

--- src/main/scala/fhj/swengb/assignments/ttt/mknaller/TicTacToeApp.scala
+++ src/main/scala/fhj/swengb/assignments/ttt/mknaller/TicTacToeApp.scala
@@ -3,3 +3,21 @@ package fhj.swengb.assignments.ttt.mknaller
 /**
   * Implement here your TicTacToe JavaFX App.
   */
+
+
+object TicTacToeApp {
+
+  def main (args: Array[String]){
+
+    val history:Map[TMove, Player] = Map()
+    println(TicTacToe.apply(history).asString())
+
+    val shistory:Map[TMove, Player] = Map(TopLeft->PlayerA, TopRight ->PlayerB)
+    println(TicTacToe.apply(shistory).asString())
+
+    for ((m, p) <- shistory) if (p==PlayerA) println("x") else println("o")
+
+  }
+
+
+}
