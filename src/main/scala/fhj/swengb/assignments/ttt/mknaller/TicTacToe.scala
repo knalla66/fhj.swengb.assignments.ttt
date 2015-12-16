8a81f1666809c40a0a8c064ef646cf06652f51f1
knalla66 <markus.knaller@edu.fh-joanneum.at>
2015-12-16 18:53:51 +0100

    so far: new functions, updated asString()

--- src/main/scala/fhj/swengb/assignments/ttt/mknaller/TicTacToe.scala
+++ src/main/scala/fhj/swengb/assignments/ttt/mknaller/TicTacToe.scala
@@ -61,12 +61,42 @@ case object PlayerB extends Player
 
 object TicTacToe {
 
+  def main (args: Array[String]){
+
+
+    val history:Map[TMove, Player] = Map()
+    println(TicTacToe.apply(history).asString())
+
+    val shistory:Map[TMove, Player] = Map(TopLeft->PlayerA, TopRight ->PlayerB)
+    println(TicTacToe.apply(shistory).asString())
+
+    for ((m, p) <- shistory) if (p==PlayerA) println("x") else println("o")
+
+    //println(TicTacToe.apply(shistory).turn(MiddleLeft,PlayerA).turn(BottomCenter, PlayerB).turn(MiddleCenter, PlayerA).asString())
+    println(TicTacToe.apply().turn(MiddleLeft,PlayerA).turn(BottomCenter, PlayerB).turn(MiddleCenter, PlayerA).asString())
+
+
+    // erstellen eines testfields
+
+    val testfield = TicTacToe.apply().turn(MiddleLeft,PlayerA).turn(BottomLeft,PlayerB).turn(MiddleCenter, PlayerA).turn(BottomRight, PlayerB)
+      .turn(BottomCenter, PlayerA).turn(MiddleRight, PlayerB)
+
+    println(testfield.moveHistory)
+
+    // test output
+    //print(testfield.asString())
+    println("REmainingmoves: " + testfield.remainingMoves)
+    println("REmainingmoves size: " + testfield.remainingMoves.size)
+    println("Remainingmoves position: " + testfield.moveHistory.keySet)
+
+  }
+
   /**
     * creates an empty tic tac toe game
     * @return
     */
-  def apply(): TicTacToe = TicTacToe()
-
+  def apply(): TicTacToe = TicTacToe(Map())
+    // wenn nur TicTacToe() benutzt wird, kann die apply funktion beim println() nicht aufgerufen werden.
   /**
     * For a given tic tac toe game, this function applies all moves to the game.
     * The first element of the sequence is also the first move.
@@ -109,21 +139,40 @@ case class TicTacToe(moveHistory: Map[TMove, Player],
     *
     * @return
     */
+/*
   def asString(): String = {
 
-    val topl = if (moveHistory.contains(TopLeft) && nextPlayer == PlayerA)  "x"  else " "
-    val topc = if (moveHistory.contains(TopCenter) && nextPlayer == PlayerA)  "x"  else " "
-    val topr = if (moveHistory.contains(TopRight) && nextPlayer == PlayerA)  "x"  else " "
-    val midl = if (moveHistory.contains(MiddleLeft) && nextPlayer == PlayerA)  "x"  else " "
-    val midc = if (moveHistory.contains(MiddleCenter) && nextPlayer == PlayerA)  "x"  else " "
-    val midr = if (moveHistory.contains(MiddleRight) && nextPlayer == PlayerA)  "x"  else " "
-    val botl = if (moveHistory.contains(BottomLeft) && nextPlayer == PlayerA)  "x"  else " "
-    val botc = if (moveHistory.contains(BottomCenter) && nextPlayer == PlayerA)  "x"  else " "
-    val botr = if (moveHistory.contains(BottomRight) && nextPlayer == PlayerA)  "x"  else " "
+    val topl = if (moveHistory.contains(TopLeft) && nextPlayer == PlayerA)  "x"  else if (moveHistory.contains(TopLeft) && nextPlayer == PlayerB) "o" else " "
+    val topc = if (moveHistory.contains(TopCenter) && nextPlayer == PlayerA)  "x" else if (moveHistory.contains(TopCenter) && nextPlayer == PlayerB) "o" else " "
+    val topr = if (moveHistory.contains(TopRight) && nextPlayer == PlayerA)  "x" else if (moveHistory.contains(TopRight) && nextPlayer == PlayerB) "o" else " "
+    val midl = if (moveHistory.contains(MiddleLeft) && nextPlayer == PlayerA)  "x" else if (moveHistory.contains(MiddleLeft) && nextPlayer == PlayerB) "o" else " "
+    val midc = if (moveHistory.contains(MiddleCenter) && nextPlayer == PlayerA)  "x" else if (moveHistory.contains(MiddleCenter) && nextPlayer == PlayerB) "o" else " "
+    val midr = if (moveHistory.contains(MiddleRight) && nextPlayer == PlayerA)  "x" else if (moveHistory.contains(MiddleRight) && nextPlayer == PlayerB) "o" else " "
+    val botl = if (moveHistory.contains(BottomLeft) && nextPlayer == PlayerA)  "x" else if (moveHistory.contains(BottomLeft) && nextPlayer == PlayerB) "o" else " "
+    val botc = if (moveHistory.contains(BottomCenter) && nextPlayer == PlayerA)  "x" else if (moveHistory.contains(BottomCenter) && nextPlayer == PlayerB) "o" else " "
+    val botr = if (moveHistory.contains(BottomRight) && nextPlayer == PlayerA)  "x" else if (moveHistory.contains(BottomRight) && nextPlayer == PlayerB) "o" else " "
 
 
     "|---|---|---|\n"+ "| "+topl+" | "+topc+" | "+topr+" |\n|---|---|---|\n"+ "| "+midl+" | "+midc+" | "+midr+" |\n|---|---|---|\n"+ "| "+botl+" | "+botc+" | "+botr+" |\n|---|---|---|"
 
+  }
+*/
+
+  def asString(): String = {
+
+    def setField(move: TMove): String = moveHistory match {
+      case notInHistory if !moveHistory.contains(move) => "   "
+      case makeX if makeX(move) == PlayerA  => " x "
+      case makeO if makeO(move)==PlayerB => " o "
+    }
+
+    "|---|---|---|\n" +
+      "|"+setField(TopLeft)+"|"+setField(TopCenter)+"|"+setField(TopRight)+"|\n" +
+      "|---|---|---|\n" +
+      "|"+setField(MiddleLeft)+"|"+setField(MiddleCenter)+"|"+setField(MiddleRight)+"|\n" +
+      "|---|---|---|\n" +
+      "|"+setField(BottomLeft)+"|"+setField(BottomCenter)+"|"+setField(BottomRight)+"|\n" +
+      "|---|---|---|"
   }
 
 
@@ -137,7 +186,8 @@ case class TicTacToe(moveHistory: Map[TMove, Player],
   /**
     * the moves which are still to be played on this tic tac toe.
     */
-  val remainingMoves: Set[TMove] = Set(TopLeft, TopCenter, TopRight, MiddleLeft, MiddleCenter, MiddleRight, BottomLeft, BottomCenter, BottomRight)
+  val allMoves: Set[TMove] = Set(TopLeft, TopCenter, TopRight, MiddleLeft, MiddleCenter, MiddleRight, BottomLeft, BottomCenter, BottomRight)
+  val remainingMoves: Set[TMove] = allMoves.diff(moveHistory.keySet)
   // alle die nicht mehr gebraucht werden können dann ja immer gedroppt werden.
 
   /**
@@ -161,8 +211,23 @@ case class TicTacToe(moveHistory: Map[TMove, Player],
     * @param player the player
     * @return
     */
-  def turn(p: TMove, player: Player): TicTacToe = ???
+  def turn(p: TMove, player: Player): TicTacToe = {
+    // überprüft ob der TMove in der moveHistory drinnen ist - wenn nicht, dann wird der MoveHistory der Move hinzugefügt
+    // mit dem jetzigen User und das TicTacToe damit aufgerufen und den nächsten player
+    //if (!moveHistory.get(p).contains(PlayerA) || !moveHistory.get(p).contains(PlayerB)){
+    if (!moveHistory.contains(p)) {
+      if (player.equals(PlayerA))
+        TicTacToe(moveHistory + (p -> player), PlayerB)
+      else player.equals(PlayerB)
+        TicTacToe(moveHistory + (p -> player), PlayerA)
 
+    }
+    // wenn der Player schon gesetzt hat und nochmals drückt
+    else {
+      TicTacToe(moveHistory, nextPlayer)
+    }
+
+  }
 }
 
 
